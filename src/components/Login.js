4a41d3c18e328b671208d971270fe22d71638ee2
viriato <viriatobonifa22@gmail.com>
2025-04-11 19:14:29 +0200

    commit erro stock6

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ export default function Login({ children }) {
   );
  var [Entrar,setEntrar] = useState("Entrar");
 
-  const endpointSpring = "  http://localhost:8080/tenant1/login";
+  const endpointSpring = " https://api.mozsystems.com/tenant1/login";
  
   const handleLogin = async () => {
     try {
--- src/pages/servicos/vendas/vendasRepositorio.js
+++ src/pages/servicos/vendas/vendasRepositorio.js
@@ -4,7 +4,7 @@ export class repositorioVenda{
 
    
     constructor(){
-      this.endpoint =" http://localhost:8080/vendas"
+      this.endpoint =" https://api.mozsystems.com/tenant2/vendas"
       this.mensagem= new mensagem();
         this.token=sessionStorage.getItem("token");
     
